7c0799bc88fc96dc47de80e080ed805b60302615
Yi De Zhu <zhu.money@gmail.com>
2021-09-19 18:59:08 -0600

    initial

---
+++ src/main/java/guru/springframework/SFGCKJoke/SfgCkJokeApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.SFGCKJoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgCkJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgCkJokeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/SFGCKJoke/SfgCkJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.SFGCKJoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgCkJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
